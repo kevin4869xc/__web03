4d5ee6c70db93fd6c86da922a44cf1caa1f378cd
kevin4869xc <kevin4869xc@gmail.com>
2022-12-24 14:16:25 +0800

    4

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
-    print(f"request的attribute method:{request.method}")
-    print(f"request的property mimetype:{request.mimetype_params}")    
     return render_template('index.html')
+
+@app.route("/name/<username>")
+def show_user(username):
+    return f"<h3>姓名:<strong>{username}</strong></h3>"
